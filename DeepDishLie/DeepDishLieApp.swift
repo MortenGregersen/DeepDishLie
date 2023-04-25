3e736bc916c8977bf8ec5a9c579e197436862814
Morten Bjerg Gregersen <759680+MortenGregersen@users.noreply.github.com>
2023-04-25 11:30:28 +0200

    Initial Commit

---
+++ DeepDishLie/DeepDishLieApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepDishLieApp.swift
+//  DeepDishLie
+//
+//  Created by Morten Bjerg Gregersen on 25/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct DeepDishLieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
