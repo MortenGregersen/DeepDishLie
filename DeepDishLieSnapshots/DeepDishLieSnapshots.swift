548dc4626dd21de14c8d7b82f9116707a8a4c88e
Morten Bjerg Gregersen <morten@atterdagapps.com>
2024-04-28 09:38:47 +0200

    Updated screenshots

--- DeepDishLieSnapshots/DeepDishLieSnapshots.swift
+++ DeepDishLieSnapshots/DeepDishLieSnapshots.swift
@@ -8,18 +8,17 @@
 import XCTest
 
 final class ScoreWondersUITests: XCTestCase {
-    func testAppStoreScreenshots() throws {
+    @MainActor func testAppStoreScreenshots() async throws {
         let app = XCUIApplication()
         app.launchArguments.append("-Demo")
         app.launchArguments.append("true")
         setupSnapshot(app)
         app.launch()
-        snapshot("1-Welcome")
-        app.buttons["Let's go!"].tap()
-        let charlie = app.staticTexts["Charlie Chapman"]
-        guard charlie.waitForExistence(timeout: 4) else { XCTFail(); return }
-        charlie.tap()
-        guard app.staticTexts["I've step foot on every US State, except Alaska"].waitForExistence(timeout: 4) else { XCTFail(); return }
-        snapshot("2-Speakers")
+        let tabBar = app.tabBars["Tab Bar"]
+        snapshot("1-Schedule")
+        tabBar.buttons["Wu with the Weather"].tap()
+        snapshot("2-Weather")
+        tabBar.buttons["About"].tap()
+        snapshot("3-About")
     }
 }
