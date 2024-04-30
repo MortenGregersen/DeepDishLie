cfcdac86a5ce41e724850270234a70399ffbcae9
Morten Bjerg Gregersen <morten@atterdagapps.com>
2024-04-30 21:08:31 +0200

    Better support for smaller screens in weather (and make it easier for Apple to approve)

--- DeepDishLie/Views/WeatherView.swift
+++ DeepDishLie/Views/WeatherView.swift
@@ -15,105 +15,125 @@ struct WeatherView: View {
 
     var body: some View {
         NavigationStack {
-            VStack {
-                if let weather = weatherController.weather {
-                    Spacer()
-                    VStack(spacing: 0) {
-                        Text("The temperature in Rosemont is")
-                        Text(weather.currentWeather.temperature.formatted(.measurement(width: .narrow, numberFormatStyle: .number.precision(.fractionLength(1)))))
-                            .font(.system(size: 80))
-                            .fontWeight(.semibold)
-                        Text("Feels like \(weather.currentWeather.apparentTemperature.formatted(.measurement(width: .narrow, numberFormatStyle: .number.precision(.fractionLength(1)))))")
-                            .font(.title2)
-                        if let attribution = weatherController.attribution {
-                            Link(destination: attribution.legalPageURL) {
-                                let imageUrl = colorScheme == .light ? attribution.combinedMarkLightURL : attribution.combinedMarkDarkURL
-                                AsyncImage(url: imageUrl) { image in
-                                    image
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(height: 10)
-                                } placeholder: {}
-                            }
+            ScrollView {
+                VStack {
+                    if let weather = weatherController.weather {
+                        VStack(spacing: 0) {
+                            Text("The temperature in Rosemont is")
+                            Text(weather.currentWeather.temperature.formatted(.measurement(width: .narrow, numberFormatStyle: .number.precision(.fractionLength(1)))))
+                                .font(.system(size: 80))
+                                .fontWeight(.semibold)
+                            Text("Feels like \(weather.currentWeather.apparentTemperature.formatted(.measurement(width: .narrow, numberFormatStyle: .number.precision(.fractionLength(1)))))")
+                                .font(.title2)
+                        }
+                        .padding(.top, 24)
+                        verdict(weather: weather)
                             .padding(.top, 8)
-                        }
-                    }
-                    Spacer()
-                    verdict(weather: weather)
-                    Spacer()
-                    Button {
-                        openURL(URL(string: "https://apps.apple.com/us/app/please-dont-rain/id6444577668")!)
-                    } label: {
-                        HStack {
-                            Image("please-dont-rain")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 70)
-                                .clipShape(RoundedRectangle(cornerRadius: 20))
-                            VStack(alignment: .leading) {
-                                Text("Get more weather info in")
-                                Text("Please don't rain")
-                                    .font(.title)
-                                    .fontWeight(.bold)
+                        Button {
+                            openURL(URL(string: "https://apps.apple.com/us/app/please-dont-rain/id6444577668")!)
+                        } label: {
+                            HStack {
+                                Image("please-dont-rain")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 70)
+                                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                                VStack(alignment: .leading) {
+                                    Text("Get more weather info in")
+                                    Text("Please don't rain")
+                                        .font(.title)
+                                        .fontWeight(.bold)
+                                }
                             }
                         }
-                    }
-                    Spacer()
-                    VStack {
-                        if weatherController.fetching {
-                            HStack(spacing: 8) {
-                                ProgressView()
-                                Text("Refreshing weather...")
-                                    .font(.callout)
+                        .padding(.top, 16)
+                        if let attribution = weatherController.attribution {
+                            VStack(spacing: 8) {
+                                Text("Forecast data provided by")
+                                    .font(.caption)
+                                Link(destination: attribution.legalPageURL) {
+                                    VStack(spacing: 4) {
+                                        let imageUrl = colorScheme == .light ? attribution.combinedMarkLightURL : attribution.combinedMarkDarkURL
+                                        AsyncImage(url: imageUrl) { image in
+                                            image
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fit)
+                                                .frame(height: 15)
+                                        } placeholder: {}
+                                        Text("and other sources")
+                                            .font(.caption)
+                                    }
+                                }
                             }
-                        } else {
+                            .padding(.top, 16)
+                        }
+                    } else if weatherController.fetching {
+                        ProgressView("Fetching weather...")
+                    } else if let errorFetching = weatherController.errorFetching {
+                        ContentUnavailableView {
+                            Label("Error fetching weather", systemImage: "thermometer.medium.slash")
+                        } description: {
+                            Text(errorFetching.localizedDescription)
+                        } actions: {
                             Button {
                                 Task { await weatherController.fetchWeather() }
                             } label: {
-                                Label("Refresh weather", systemImage: "arrow.clockwise")
-                                    .font(.callout)
+                                Label("Try again", systemImage: "arrow.clockwise")
+                                    .padding(4)
                             }
+                            .buttonStyle(.borderedProminent)
                         }
-                    }
-                    .frame(minHeight: 50)
-                } else if weatherController.fetching {
-                    ProgressView("Fetching weather...")
-                } else if let errorFetching = weatherController.errorFetching {
-                    ContentUnavailableView {
-                        Label("Error fetching weather", systemImage: "thermometer.medium.slash")
-                    } description: {
-                        Text(errorFetching.localizedDescription)
-                    } actions: {
-                        Button {
-                            Task { await weatherController.fetchWeather() }
-                        } label: {
-                            Label("Try again", systemImage: "arrow.clockwise")
-                                .padding(4)
-                        }
-                        .buttonStyle(.borderedProminent)
                     }
                 }
             }
+            .overlay(alignment: .bottom) {
+                if weatherController.weather != nil, weatherController.fetching {
+                    HStack(spacing: 8) {
+                        ProgressView()
+                        Text("Refreshing weather...")
+                            .font(.callout)
+                    }
+                    .padding(8)
+                    .padding(.horizontal, 4)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.accentColor.opacity(0.3))
+                    )
+                    .padding(.bottom)
+                }
+            }
             .navigationTitle("Wu with the Weather")
+            .navigationBarTitleDisplayMode(.automatic)
             .toolbarBackground(Color.accentColor, for: .navigationBar)
             .toolbarBackground(.visible, for: .navigationBar)
             .toolbarColorScheme(.dark, for: .navigationBar)
+            .toolbar {
+                Button {
+                    Task { await weatherController.fetchWeather() }
+                } label: {
+                    Label("Refresh weather", systemImage: "arrow.clockwise")
+                        .font(.callout)
+                }
+                .disabled(weatherController.weather == nil || weatherController.fetching)
+            }
         }
     }
 
     @ViewBuilder private func verdict(weather: Weather) -> some View {
-        let info = if weather.currentWeather.temperature.converted(to: .fahrenheit).value < 70 {
-            (emoji: "🥶", text: "Chris Wu doesn't like this weather... 👎")
-        } else {
-            (emoji: "💚", text: "Chris Wu loves this weather! 👍")
+        VStack(spacing: 0) {
+            let info = if weather.currentWeather.temperature.converted(to: .fahrenheit).value < 70 {
+                (emoji: "🥶", text: "Chris Wu doesn't like this weather... 👎")
+            } else {
+                (emoji: "💚", text: "Chris Wu loves this weather! 👍")
+            }
+            Text(info.emoji)
+                .font(.system(size: 80))
+            Text(info.text)
+                .font(.title)
+                .fontWeight(.black)
+                .padding(.horizontal, 50)
+                .multilineTextAlignment(.center)
         }
-        Text(info.emoji)
-            .font(.system(size: 80))
-        Text(info.text)
-            .font(.title)
-            .fontWeight(.black)
-            .padding(.horizontal, 50)
-            .multilineTextAlignment(.center)
     }
 }
 
