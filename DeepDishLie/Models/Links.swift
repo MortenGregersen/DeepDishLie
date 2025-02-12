8f9622f6b0396ebc63cd43d84761f264822685ce
Morten Bjerg Gregersen <morten@atterdagapps.com>
2025-02-12 11:15:59 +0100

    Add support for Bluesky and Threads links

--- DeepDishLie/Models/Links.swift
+++ DeepDishLie/Models/Links.swift
@@ -9,8 +9,10 @@ import Foundation
 
 struct Links: Decodable {
     let name: String?
+    let bluesky: URL?
     let github: URL?
     let mastodon: URL?
+    let threads: URL?
     let twitter: URL?
     let youtube: URL?
     let website: URL?
