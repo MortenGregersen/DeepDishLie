0af6e601d9cbf7e98ef512e3b7cb41962c303df9
Matt Heaney <MattHeaney23@gmail.com>
2025-02-22 18:47:01 +0000

    Needed restructure app clip

---
+++ DeepDishLie/Extensions/Event+Colors.swift
@@ -0,0 +1,35 @@
+//
+//  Event+Colors.swift
+//  DeepDishLie
+//
+//  Created by Matt Heaney on 22/02/2025.
+//
+
+import SwiftUI
+import DeepDishCore
+
+extension Event {
+    var dateTextColor: Color {
+        if isHappeningNow {
+            .init(uiColor: UIColor.lightText)
+        } else {
+            .accentColor
+        }
+    }
+
+    var titleTextColor: Color {
+        if isHappeningNow {
+            .init(uiColor: UIColor.systemBackground)
+        } else {
+            toBeDetermined ? .secondary : .primary
+        }
+    }
+
+    var subtitleTextColor: Color {
+        if isHappeningNow {
+            .init(uiColor: UIColor.systemBackground)
+        } else {
+            .secondary
+        }
+    }
+}
