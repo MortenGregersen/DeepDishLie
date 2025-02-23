e5a33b85077ce76e9dba1fda1958587226de8ad8
Matt Heaney <129856192+MattHeaney23@users.noreply.github.com>
2025-02-23 19:35:01 +0000

    Schedule App Clip (#19)
    * Needed restructure app clip
    
    * Base of app clip set up
    
    Working provisioning profiles
    
    Small fix
    
    Small fixes
    
    Fixes
    
    More fixes
    
    * Added the launch screen image to the app clip
    
    ---------
    
    Co-authored-by: Matt Heaney <MattHeaney23@gmail.com>

---
+++ ScheduleAppClip/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  ScheduleAppClip
+//
+//  Created by Matt Heaney on 22/02/2025.
+//
+
+import SwiftUI
+import DeepDishCore
+
+struct ContentView: View {
+
+    @State private var settingsController = SettingsController()
+    @State private var scheduleController = ScheduleController()
+
+    var body: some View {
+        NavigationView {
+            ScheduleListView()
+                .navigationTitle("Deep Dish Swift 🍕")
+                .toolbarBackground(Color.navigationBarBackground,
+                                   for: .navigationBar)
+                .toolbarBackground(.visible, for: .navigationBar)
+                .toolbarColorScheme(.dark, for: .navigationBar)
+        }
+        .environment(settingsController)
+        .environment(scheduleController)
+    }
+}
+
+#Preview {
+    ContentView()
+}
