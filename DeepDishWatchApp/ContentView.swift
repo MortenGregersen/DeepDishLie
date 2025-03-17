9acd1f5b0408b5a3c07deb6b69389b61ffabafbe
Morten Bjerg Gregersen <morten@atterdagapps.com>
2025-03-17 21:36:09 +0100

    Add watch app target

---
+++ DeepDishWatchApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DeepDishWatchApp
+//
+//  Created by Morten Bjerg Gregersen on 17/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DeepDishWatchApp/DeepDishWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepDishWatchApp.swift
+//  DeepDishWatchApp
+//
+//  Created by Morten Bjerg Gregersen on 17/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct DeepDishWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
