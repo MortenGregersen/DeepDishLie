59bff133f178f6c2f2cf8a7b6696da87f73dbf75
Morten Bjerg Gregersen <morten@atterdagapps.com>
2025-03-17 21:56:05 +0100

    Add app core framework

---
+++ DeepDishAppCore/DeepDishAppCore.h
@@ -0,0 +1,18 @@
+//
+//  DeepDishAppCore.h
+//  DeepDishAppCore
+//
+//  Created by Morten Bjerg Gregersen on 17/03/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DeepDishAppCore.
+FOUNDATION_EXPORT double DeepDishAppCoreVersionNumber;
+
+//! Project version string for DeepDishAppCore.
+FOUNDATION_EXPORT const unsigned char DeepDishAppCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeepDishAppCore/PublicHeader.h>
+
+
